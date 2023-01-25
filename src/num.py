9aef9e551c8a2e66e59242745a1aeced8381da24
sankettangade <sankettangade99@gmail.com>
2023-01-25 16:24:39 -0500

    Added Sym and Num classes

---
+++ src/num.py
@@ -0,0 +1,45 @@
+import math
+
+class Num:
+
+    def __init__(self, txt, at):
+        self.n = 0
+        self.mu = 0
+        self.m2 = 0
+        self.hi = math.inf
+        self.lo = -math.inf
+
+        if not txt: self.txt = ""
+        else:
+            self.txt = txt
+            if self.txt[-1] == '-': self.w = -1
+            else: self.w = 1
+
+
+        if at: self.at = at
+        else: self.at = 0
+
+    def add(self, n):
+        '''
+        Addition method for n
+        '''
+        if n != "?":
+            self.n = self.n + 1
+            d = n - self.mu
+            self.mu = self.mu + (d / self.n)
+            self.m2 = self.m2 + (d * (n - self.mu))
+            self.lo = min(n, self.lo)
+            self.hi = max(n, self.hi)
+
+    def mid(self):
+        '''
+        Method for calculating mean
+        '''
+        return self.mu
+
+    def div(self):
+        '''
+        Method for calculating Standard Deviation
+        '''
+        return (self.m2 < 0 or self.n < 2) and 0 or (self.m2 / (self.n - 1)) ** 0.5
+
