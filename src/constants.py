d355f8b7c982eee1ed589a8eebfcb9b9a919d4f7
tilaksatra <ayushsatra@gmail.com>
2023-01-31 20:24:04 -0500

    Added, Integrated Test Engine, Test Workflow and output image

--- src/constants.py
+++ src/constants.py
@@ -18,7 +18,7 @@ help = '''
 USAGE: testengine.py  [OPTIONS] [-g ACTION]
 OPTIONS:
     -d  --dump  on crash, dump stack = false
-    -f  --file  name of file         = ../etc/data/auto93.csv
+    -f  --file  name of file         = ./etc/data/auto93.csv
     -g  --go    start-up action      = data
     -h  --help  show help            = false
     -s  --seed  random number seed   = 937162211
